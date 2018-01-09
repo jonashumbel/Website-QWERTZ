e5d27a2655389ffdf1e0643d2e584b09a22b20e7
jonashumbel <jonas.humbel@gmx.ch>
2018-01-09 08:41:47 +0100

    event page adjustments

--- src/pages/event/event.js
+++ src/pages/event/event.js
@@ -1,10 +1,11 @@
-define(['knockout', 'text!./event.html'], function(ko, templateMarkup) {
+define(['knockout', 'text!./event.html', 'jquery'], function(ko, templateMarkup, $) {
 
   function Event(params) {
     var self = this;
     self.registerChoice = ko.observable('');
     self.addPerson = ko.observable(false);
     self.ownValue = ko.observable('');
+
   }
 
   // This runs when the component is torn down. Put here any logic necessary to clean up,
